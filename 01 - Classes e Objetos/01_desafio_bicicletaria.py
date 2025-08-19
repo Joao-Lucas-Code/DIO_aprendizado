6c7fe7ccf8d0c7e4b81358f1f929baaa56977aa6
João Lucas <joao.lgomes1166@gmail.com>
2025-08-19 18:28:11 -0300

    Classes

---
+++ 01 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -0,0 +1,31 @@
+class Bicicleta:
+    def __init__(self,cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+    
+    def buzinar(self):
+        print("Plim plim...")
+
+    def parar(self):
+        print("Parando a bicicleta...")
+        print("bicicleta parada!")
+
+    def correr(self):
+        print("Vrummmm...")
+
+    # def __str__(self):
+    #     return f"Bicicleta: cor = {self.cor}, modelo = {self.modelo}, ano = {self.ano}, valor = {self.valor}" # tem que colocar na mao
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}'for chave, valor in self. # Nao tem que colocar na mao
+        __dict__.items()])} "
+
+b1 = Bicicleta("preta", "caloi", 2025, 1200)
+# b1.buzinar()
+# b1.correr()
+# b1.parar()
+
+# print(b1.cor, b1.modelo, b1.valor, b1.ano)
+print(b1)
